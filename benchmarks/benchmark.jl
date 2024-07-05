f029c42671c9bd588633f53c7228811f7b329255
Dominique Orban <dominique.orban@gmail.com>
2024-07-05 17:09:28 -0400

    update dependencies

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -11,11 +11,10 @@ using ReverseADNLSModels
 
 df = problems_df()
 filter_df = df[ ( df.nequ .≥ 50000 ) .& ( df.nvar .≤ 34000 ), :]
-name, group = get_first_name_and_group(filter_df)
-path = fetch_ba_name(name, group)
-filename = BundleAdjustmentModels.get_filename(name, group)
-filedir = BundleAdjustmentModels.fetch_ba_name(filename, group)
-path_and_filename = joinpath(filedir, filename)
+name = filter_df[1, :name]
+group = filter_df[1, :group]
+path = fetch_ba_name(name)
+path_and_filename = joinpath(path, name * ".txt.bz2")
 T = Float64
 cams_indices, pnts_indices, pt2d, x0, ncams, npnts, nobs = BundleAdjustmentModels.readfile(path_and_filename, T = T)
 nvar = 9 * ncams + 3 * npnts
@@ -24,17 +23,17 @@ nequ = 2 * nobs
 # residual function for evaluation
 k = similar(x0)
 P1 = similar(x0)
-resid!(rx, x) = BundleAdjustmentModels.residuals!(cams_indices, pnts_indices, x, rx, nobs, npnts, k, P1)
+resid!(rx, x) = BundleAdjustmentModels.residuals!(x, rx, cams_indices, pnts_indices, nobs, npnts, pt2d)
 
 # residual function for forward AD
 k_fwd = Vector{SparseDiffTools.Dual{ForwardDiff.Tag{SparseDiffTools.DeivVecTag, T}, T, 1}}(undef, nvar)
 P1_fwd = Vector{SparseDiffTools.Dual{ForwardDiff.Tag{SparseDiffTools.DeivVecTag, T}, T, 1}}(undef, nvar)
-resid_fwd!(rx, x) = BundleAdjustmentModels.residuals!(cams_indices, pnts_indices, x, rx, nobs, npnts, k_fwd, P1_fwd)
+resid_fwd!(rx, x) = BundleAdjustmentModels.residuals!(x, rx, cams_indices, pnts_indices, nobs, npnts, pt2d)
 
 # residual function for reverse AD
 k_rev = Vector{ReverseDiff.TrackedReal{T, T, Nothing}}(undef, nvar)
 P1_rev = Vector{ReverseDiff.TrackedReal{T, T, Nothing}}(undef, nvar)
-resid_rev!(rx, x) = BundleAdjustmentModels.residuals!(cams_indices, pnts_indices, x, rx, nobs, npnts, k_rev, P1_rev)
+resid_rev!(rx, x) = BundleAdjustmentModels.residuals!(x, rx, cams_indices, pnts_indices, nobs, npnts, pt2d)
 
 rd_bamodel = ReverseADNLSModel(resid!, nequ, x0, r_fwd! = resid_fwd!, r_rev! = resid_rev!)
 
